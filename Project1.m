7fb3cdb96554ab43430ef9fdf899c69f4e6f3e33
Shiven Sasipalli <115129043+Shiven0011@users.noreply.github.com>
2023-03-13 10:46:46 -0400

    Initial Code

---
+++ Project1.m
@@ -0,0 +1,21 @@
+
+cipher = input('What cipher would you like us to decode? The input /n');
+
+
+
+Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+map(Alphabet) = 1:length(Alphabet);
+put_in = map(to_decrypt)
+
+if cipher == "Vignere"
+
+end
+
+if cipher == "Ceaser"
+
+end
+
+if cipher == "Hill"
+
+end
+
