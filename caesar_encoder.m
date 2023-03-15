b6918339333a09336fde09cff1ac7982926c1855
sudata5 <127125495+sudata5@users.noreply.github.com>
2023-03-15 14:32:25 -0400

    Caesar encoder - works
    takes in text input, converts to number (using let2num), then encodes using a random shift

---
+++ caesar_encoder.m
@@ -0,0 +1,24 @@
+function [encoded] = caesar_encoder(text)
+%CAESAR_ENCODER shifts the entire message by the same scalar amount. 
+
+%   let2num function
+Alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+Map(Alphabet(1:26)) = 1:26;
+message = Map(text);
+%   'message' is a vector generated from the letters in 'text'.
+
+shift = randi([1,26], 1, 1);
+%   'shift' is a random scalar that is used to shift the caesar cypher.
+
+encoded = message + shift;
+
+for ii = 1:length(message)
+    if encoded(ii) >26
+        encoded(ii) = encoded(ii) - 26;
+    end
+end
+
+disp(encoded);
+
+end
+
