0c95176d299d13a17314ba9e04f12ba9a316bb40
Shiven Sasipalli <115129043+Shiven0011@users.noreply.github.com>
2023-03-15 10:49:14 -0400

    Expanded and potential mapping code added

--- Project1.m
+++ Project1.m
@@ -1,21 +1,28 @@
+% By Shiven Sasipalli
+% Time spent on this section: 2 hrs
 
-cipher = input('What cipher would you like us to decode? The input /n');
+cipher = cast(input('What cipher would you like us to decode? The input \n'), char);
 
 
+to_decrypt = cast(input('What is it that needs to be decrypted? \n'), char);
 
-Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-map(Alphabet) = 1:length(Alphabet);
-put_in = map(to_decrypt)
 
-if cipher == "Vignere"
+function [decrypted] = decryption(cipher, to_decrypt)
 
-end
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+Map(Alphabet) = 1:length(Alphabet);
+Map(to_decrypt)
 
-if cipher == "Ceaser"
-
-end
-
-if cipher == "Hill"
+if cipher == 'Vignere'
 
 end
 
+if cipher == 'Ceaser'
+
+end
+
+if cipher == 'Hill'
+
+end
+
+end
